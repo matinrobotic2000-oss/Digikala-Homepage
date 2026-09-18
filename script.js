68bf6f2c0cec57dfa5a3ca3e1239590120eb31c5
matinTheGreat <matinrobotic2000@gmail.com>
2026-09-18 11:49:34 +0330

    Implement next & back btn hero section

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const homepageHidden = document.querySelector('#homepage-hidden');
 const loginPage = document.querySelector('.login-page');
 const closeBtn = document.querySelector('.close-btn');
 
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+
+const nextBtn = document.querySelector('.next-btn');
+const backBtn = document.querySelector('.back-btn');
+
+let currentSlide = 0;
+const totalSlides = slides.length;
+
 const openLoginPage = function (e) {
   e.preventDefault();
   console.log('click');
@@ -20,3 +29,23 @@ const closeLoginPage = function (e) {
 
 loginBtn.addEventListener('click', openLoginPage.bind(this));
 closeBtn.addEventListener('click', closeLoginPage.bind(this));
+
+const gotoSlide = function (currentSlide) {
+  slides.forEach(s => {
+    s.style.transform = `translateX(${100 * currentSlide}%)`;
+  });
+};
+
+gotoSlide(0);
+
+nextBtn.addEventListener('click', function () {
+  if (currentSlide === totalSlides - 1) currentSlide = 0;
+  else currentSlide++;
+  gotoSlide(currentSlide);
+});
+
+backBtn.addEventListener('click', function () {
+  if (currentSlide === 0) currentSlide = totalSlides - 1;
+  else currentSlide--;
+  gotoSlide(currentSlide);
+});
